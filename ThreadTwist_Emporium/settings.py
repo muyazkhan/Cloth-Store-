86c2b5906385d1950d0a61fff93bfb02d270ceab
Muyaz khan <muyazkhan06@gmail.com>
2024-01-31 10:09:31 +0600

    reuplode

--- ThreadTwist_Emporium/settings.py
+++ ThreadTwist_Emporium/settings.py
@@ -137,8 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'product_list','media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
